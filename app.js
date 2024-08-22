1592ce17ab6a31defa00cea0f401f56e9947c27b
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2024-08-22 20:03:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,5 +46,5 @@ app.post("/short", async(req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${PORT}`);
 });
