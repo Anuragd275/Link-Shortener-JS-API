e34de223d5ec72e1b9cbe42ad09b2924153be894
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2024-09-07 22:48:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,40 @@
-const express = require("express")
-const createHash = require("./middleware/hasher");
-const isURLThere = require("./middleware/validate_url")
-const verifyShortID = require("./middleware/verifyShortID")
-
+const express = require("express");
 const app = express();
+const mapURL = require("./middleware/urlMapper")
+const scrapeThuttu = require("./middleware/thuttuScraper/scrapper")
+const bot = require("./middleware/thuttuScraper/bot");
+const checkAndSend = require("./middleware/thuttuScraper/bot");
+const path = require('path');
 
 require('dotenv').config();
 
-// Middleware to parse JSON bodies
-app.use(express.json());
 
-// Middleware to parse URL-encoded bodies (from forms)
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    res.send("Welcome to Home page")    
-})
-
-
-app.get("/hi", (req, res) => {
-    res.send("Hi page!!")
-})
-
-
-app.get("/:short_id", async(req, res) => {
-
-    const short_id = req.params.short_id
-    const long_url = await verifyShortID(short_id)
-
-    res.send(long_url)
-})
-
-// POST METHOD, GIVE LONG URL RECEIVE SHORT URL
-
-app.post("/short", async(req, res) => {
-    console.log(req.body);
-    result = createHash(req.body.long_url)
-    console.log(result)
-    db_result = await isURLThere(result.short_id, result.long_url)
-    console.log(db_result)
-    res.send(db_result);
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get("/:shortID", async (req, res) => {
+    let url_short_id = req.params.shortID;
 
-const PORT = process.env.PORT || 3000;
+    console.log("Short URL id: ", url_short_id);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    const long_url = await mapURL(url_short_id);
+
+    if (long_url) {
+
+        console.log("Long URL: ", long_url);
+
+        await checkAndSend();
+
+        res.redirect(301, long_url);
+
+        console.log(`${url_short_id} ----> ${long_url}`);
+        
+    } else {
+        res.status(404).json({ error: "Short URL not found" });
+    }
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`))
