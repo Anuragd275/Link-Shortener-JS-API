cf2ac32b70091d04d248c1aca7bd0c0507675d03
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2026-02-02 00:51:00 +0530

    Update views directory path to use path.join

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(cors({
     origin: '*', // Allow all origins
