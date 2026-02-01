ff730812679857b414363e5005a9c9c06b9dbefe
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2026-02-02 00:41:36 +0530

    Set view engine to Pug and define views directory

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const cors = require('cors');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(cors({
     origin: '*', // Allow all origins
     methods: ['GET', 'POST'],
